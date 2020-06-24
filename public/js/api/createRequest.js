69cb29ad95a6d874afa101a8fc6a578ac50f1d73
Antis85 <55399269+Antis85@users.noreply.github.com>
2020-06-24 15:54:22 +0300

    createRequest.js done

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -1,7 +1,39 @@
+"use strict";
+
 /**
  * Основная функция для совершения запросов
  * на сервер.
  * */
-const createRequest = (options = {}) => {
 
+const createRequest = (options = {}) => {
+  let xhr = new XMLHttpRequest();
+  xhr.responseType = options.responseType;
+  xhr.withCredentials = true;
+
+  let formData = new FormData();
+  formData.append("mail", `${options.data.email}`);
+  formData.append("password", `${options.data.password}`);
+
+  try {
+    if (options.method == "GET") {
+      xhr.open(`${options.method}`, `${options.url}?mail=${options.data.email}&password=${options.data.password}`);
+      xhr.send();
+    } else {
+      xhr.open(`${options.method}`, `${options.url}`);
+      xhr.send(formData);
+    }
+
+  } catch (e) {
+    options.callback(e);
+  }
+
+  xhr.addEventListener("readystatechange", function () {
+    if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+      options.callback(null, JSON.parse(xhr.responseText));
+    } else {
+      options.callback(err);
+    }
+  });
+
+  return xhr;
 };
