b907b631a66b02d24ffc712e0667c036a9b73779
Antis85 <55399269+Antis85@users.noreply.github.com>
2020-06-26 22:21:08 +0300

    createRequest.js fix1

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -1,24 +1,21 @@
 "use strict";
-
 /**
  * Основная функция для совершения запросов
  * на сервер.
  * */
-
 const createRequest = (options = {}) => {
   let xhr = new XMLHttpRequest();
   xhr.responseType = options.responseType;
   xhr.withCredentials = true;
 
-  let formData = new FormData();
-  formData.append("mail", `${options.data.email}`);
-  formData.append("password", `${options.data.password}`);
-
   try {
     if (options.method == "GET") {
       xhr.open(`${options.method}`, `${options.url}?mail=${options.data.email}&password=${options.data.password}`);
       xhr.send();
-    } else {
+    } else {      
+      let formData = new FormData();
+      formData.append("mail", `${options.data.email}`);
+      formData.append("password", `${options.data.password}`);
       xhr.open(`${options.method}`, `${options.url}`);
       xhr.send(formData);
     }
