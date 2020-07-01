287545f99427b4c62e01978d568b1e55154950c9
Antis85 <55399269+Antis85@users.noreply.github.com>
2020-07-01 22:47:57 +0300

    CreateAccountForm.js done

--- public/js/ui/forms/CreateAccountForm.js
+++ public/js/ui/forms/CreateAccountForm.js
@@ -1,16 +1,27 @@
+"use strict";
+/*Форма CreateAccountForm отвечает за следующие функции:
+Отправляет запрос на создание счёта через Account.create
+Закрывает окно, в котором находится форма при успешном ответе
+Сбрасывает форму и вызывает App.update() при успешном ответе*/
 /**
  * Класс CreateAccountForm управляет формой
  * создания нового счёта
- * Наследуется от AsyncForm
+ * Наследуется от AsyncForm   
  * */
-class CreateAccountForm {
+class CreateAccountForm extends AsyncForm {
   /**
    * Создаёт счёт с помощью Account.create и закрывает
    * окно (в котором находится форма) в случае успеха,
    * а также вызывает App.update()
    * и сбрасывает форму
    * */
-  onSubmit( options ) {
-
+  onSubmit(options) {
+    Account.create(options.data, (err, response) => {
+      if (response && response.success) {
+        App.getModal("createAccount").close();
+        App.update();
+        this.element.reset();
+      }
+    })
   }
 }
