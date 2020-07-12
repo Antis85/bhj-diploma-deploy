2f0776bbdc0defbca95f7758f5adb43c1824551e
Antis85 <55399269+Antis85@users.noreply.github.com>
2020-07-12 16:11:27 +0300

    createRequest.js fix3

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,42 +3,51 @@
  * Основная функция для совершения запросов
  * на сервер.
  * */
-const createRequest = (options = {}) => {  
+const createRequest = (options = {}) => {
   let xhr = new XMLHttpRequest();
   xhr.responseType = options.responseType;
-  xhr.withCredentials = true;  
+  xhr.withCredentials = true;
   let formData = new FormData();
   let urlString = options.url;
+
   if (options.data) {
     urlString += "?";
     for (let key in options.data) {
       urlString += key + "=" + options.data[key] + "&";
       formData.append(key, options.data[key]);
     }
-    urlString = urlString.slice(0, -1);    
+    urlString = urlString.slice(0, -1);
+
+  } else {
+    options.data = {};
+    urlString = "";
   }
 
   try {
     if (options.method == "GET") {
-      xhr.open(options.method, urlString);     
+      xhr.open(options.method, urlString);
       xhr.send();
 
-    } else {      
+    } else {
       xhr.open(options.method, options.url);
-      xhr.send(formData);      
+      xhr.send(formData);
     }
 
   } catch (e) {
     options.callback(e);
   }
-  
-  xhr.addEventListener("readystatechange", function () {
-    if (xhr.readyState === xhr.DONE && xhr.status === 200) {
-      options.callback(null, xhr.response);
-    } else {
-      options.callback(err);
-    }
-  });
+
+  try {
+    xhr.addEventListener("readystatechange", function () {
+      if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+        options.callback(null, xhr.response);
+      }
+    });
+
+  } catch (err) {
+    options.callback(err);
+  }
 
   return xhr;
 };
+
