596c5815fd97e22dcb5c09409951b1cc6c8aa53e
Antis85 <55399269+Antis85@users.noreply.github.com>
2020-08-09 21:12:42 +0300

    Add files via upload

---
+++ docs/js/api/Transaction.js
@@ -0,0 +1,13 @@
+/**
+ 
+* Класс Transaction наследуется от Entity.
+ 
+* Управляет счетами пользователя.
+ 
+* Имеет свойство URL со значением '/transaction'
+ 
+* */
+
+class Transaction extends Entity {
+  static URL = "/transaction";
+}
